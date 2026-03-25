952a033a12ff6792909eb5b96e649d0acb20fb77
aoife00 <20117133@setu.ie>
2026-03-25 14:09:24 +0000

    First commint

---
+++ routes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import express from 'express';
+const router = express.Router();
+import logger from "./utils/logger.js";
+
+import start from './controllers/start.js';
+import dashboard from './controllers/dashboard.js';
+import about from './controllers/about.js';
+import stats from './controllers/stats.js';
+import playlist from './controllers/playlist.js';
+
+router.get('/', start.createView);
+router.get('/dashboard', dashboard.createView);
+router.get('/about', about.createView);
+router.get('/playlist/:id', playlist.createView);
+router.post('/playlist/:id/addsong', playlist.addSong);
+router.post('/dashboard/addplaylist', dashboard.addPlaylist);
+router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
+router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
+router.post('/playlist/:id/updatesong/:songid', playlist.updateSong);
+router.get('/stats', stats.createView);
+
+router.get('/error', (request, response) => response.status(404).end('Page not found.'));
+
+export default router;
